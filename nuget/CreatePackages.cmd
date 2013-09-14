faba3e859ad68e6781e2285f836a6cdc682523d9
mendel <none@none>
2013-09-14 15:26:17 +0200

    Added nuget package

---
+++ nuget/CreatePackages.cmd
@@ -0,0 +1,3 @@
+NuGet.exe pack ..\ReactiveTables.Framework.Protobuf\ReactiveTables.Framework.Protobuf.csproj -Prop Configuration=Release -IncludeReferencedProjects
+
+NuGet.exe pack ..\ReactiveTables.Framework\ReactiveTables.Framework.csproj -Prop Configuration=Release -IncludeReferencedProjects
